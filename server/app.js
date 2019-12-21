3cf80f63b515ad1139d26db773cb46233171152a
Jonathan <jonacom@lissismore.com>
2019-12-21 01:16:24 -0700

    port is always 4000

--- server/app.js
+++ server/app.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const http = require("http");
 const socketIo = require("socket.io");
-const port = process.env.PORT || 4000;
+// const port = process.env.PORT || 4000;
+const port = 4000;
 const index = require("./routes/index");
 const app = express();
 app.use(index);
